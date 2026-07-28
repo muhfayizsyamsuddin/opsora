29638e32bda51c999b01f92a14a3166c839036cf
muhfayizsyamsuddin <muhfayiz14@gmail.com>
2026-07-28 13:13:14 +0800

    feat(api): setup prisma client and user model

---
+++ apps/api/src/test-prisma.ts
@@ -0,0 +1,13 @@
+import { prisma } from "./lib/prisma.js";
+
+async function main() {
+  const users = await prisma.user.findMany();
+
+  console.log(users);
+}
+
+main()
+  .catch(console.error)
+  .finally(async () => {
+    await prisma.$disconnect();
+  });
